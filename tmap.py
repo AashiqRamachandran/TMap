4581c5ed8f16429e69771bad5eb9b68cbdd96477
Aashiq Ramachandran <ramachandran.aashiq@gmail.com>
2020-06-22 18:24:52 +0530

    Update tmap.py

--- tmap.py
+++ tmap.py
@@ -1,14 +1,16 @@
 import turtle
 from tinder_api_sms import *
-FONT = ("Arial", 12, "normal")
-t = turtle.Turtle()
+#sFONT = ("Arial", 12, "normal")
+#t = turtle.Turtle()
 recs = get_recommendations()["results"];
 for person in recs:
     person_name=person["name"]
     person_id = person["_id"];
     person_bio = person["bio"];
     person_distance=person["distance_mi"]
-    t.circle(person_distance)
-    t.write(person_name, align="center", font=FONT)
-    #print(person_bio);
-    #print(person_distance)
+    print("Name: "+person_name)
+    print("Bio is: "+str(person_bio));
+    print("is "+str(person_distance)+" miles away")
+    print("*******************************")
+    #t.circle(person_distance)
+    #t.write(person_name, align="center", font=FONT)
