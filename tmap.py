957ebdeff3b6992d59246a63b2b134bb35a73748
Aashiq Ramachandran <ramachandran.aashiq@gmail.com>
2020-06-22 14:23:19 +0530

    Update tmap.py

--- tmap.py
+++ tmap.py
@@ -1,8 +1,11 @@
+import turtle
 from tinder_api_sms import *
+t = turtle.Turtle()
 recs = get_recommendations()["results"];
 for person in recs:
     person_id = person["_id"];
     person_bio = person["bio"];
     person_distance=person["distance_mi"]
-    print(person_bio);
-    print(person_distance)
+    t.circle(distance_mi)
+    #print(person_bio);
+    #print(person_distance)
