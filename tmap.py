bfb6269336e9b098c3d622c9acaba97ed017a36c
Aashiq Ramachandran <ramachandran.aashiq@gmail.com>
2020-06-23 17:33:50 +0530

    Update tmap.py

--- tmap.py
+++ tmap.py
@@ -1,16 +1,17 @@
-import turtle
 from tinder_api_sms import *
-#sFONT = ("Arial", 12, "normal")
-#t = turtle.Turtle()
+import gmplot 
+
 recs = get_recommendations()["results"];
+mymap = gmplot.GoogleMapPlotter(13.0817, 80.2230, 16)
+#mymap.apikey="API GOES HERE"
 for person in recs:
-    person_name=person["name"]
+    person_name=person["name"];
     person_id = person["_id"];
     person_bio = person["bio"];
-    person_distance=person["distance_mi"]
+    person_distance=person["distance_mi"];
+    mymap.circle(13.0695, 80.2318,radius=int(person_distance)*1000)#, color='yellow')
+    mymap.draw('tinder_visualization.html')
     print("Name: "+person_name)
     print("Bio is: "+str(person_bio));
     print("is "+str(person_distance)+" miles away")
     print("*******************************")
-    #t.circle(person_distance)
-    #t.write(person_name, align="center", font=FONT)
